1b5011523e95563cb088de84d1f457fc4a3e328d
SamsonGee <88246655+SamsonGee@users.noreply.github.com>
2021-10-14 15:16:08 -0500

    that was a doozy

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+//this is the same as 
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+// it is pushing the array objects from profileDataArgs (which captures what is received from terminal when node is processed) 
+// and then assigned it to the corresponding array variable titles respectively 
+
+
+// console.log(profileDataArgs);
+
+// const printProfileData = profileDataArr => {
+//     // This...
+//     // for (let i = 0; i < profileDataArr.length; i += 1) {
+//     //   console.log(profileDataArr[i]);
+//     // }
   
-    console.log('================');
+//     // console.log('================');
   
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
 
-  };
+//   };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+//cut the generatePage function and added it to ./src/page-template
+
+// `Name: ${userName}, Github: ${githubName}`;
+
+// console.log(generatePage('Jane','janehub'));
+// console.log(name, github);
+// console.log(generatePage(name, github));
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
