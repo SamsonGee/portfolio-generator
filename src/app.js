758da1d3c644945946fb9671eecd96879231f691
SamsonGee <88246655+SamsonGee@users.noreply.github.com>
2021-10-20 03:40:13 -0500

    adding everything

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
+
 
 const promptUser = () => {
   return inquirer.prompt([
